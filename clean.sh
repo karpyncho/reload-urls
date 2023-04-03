90f177339c136a03571414d0eaadb6fd707278dd
Sebastian Quiles <sequiles@mpf.gov.ar>
2023-04-02 22:26:16 -0300

    primera versión vacía

---
+++ clean.sh
@@ -0,0 +1,19 @@
+
+[[ -n $(find . -name "*.pyc") ]] && echo "Cleaning *.pyc files" && find . -name "*.pyc" -type f -delete
+
+[ -d .mypy_cache ] && echo "Deleting .mypy_cache directory" && rm -rf .mypy_cache
+
+[ -d .pytest_cache ] && echo "Deleting .pytest_cache directory" && rm -rf .pytest_cache
+
+[ -d htmlcov ] && echo "Deleting htmlcov directory" && rm -rf htmlcov
+
+[ -d dist ] && echo "Deleting dist directory" && rm -rf dist
+
+[ -d src/.pytest_cache ] && echo "Deleting src/.pytest_cache directory" && rm -rf src/.pytest_cache
+
+[ -d src/*.egg-info ] && echo "Deleting src/*.egg-info directory" && rm -rf src/*.egg-info
+
+[ -f .coverage ] && echo "Deleting .coverage file" && rm .coverage
+
+[ -f coverage.xml ] && echo "Deleting coverage.xml file" && rm coverage.xml
+
---
+++ src/tests/test_init.py
@@ -0,0 +1,8 @@
+
+from karpyncho.reload_urls import *
+from unittest import TestCase
+
+
+class TestPrint(TestCase):
+    def test_ok(self):
+        pass
